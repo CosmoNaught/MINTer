cbae6387bf337204a55c732e7c69750949e327a7
OJWatson <o.watson15@imperial.ac.uk>
2025-07-19 13:27:33 +0100

    specifiy dependency locations in gha and set macos threading

--- tests/testthat/helpers.R
+++ tests/testthat/helpers.R
@@ -5,3 +5,15 @@ skip_if_no_python_pkgs <- function(pkgs = c("numpy", "pandas", "torch", "sklearn
     }
   })
 }
+
+mac_thread_safe_setup <- function() {
+  if (Sys.info()[["sysname"]] == "Darwin") {
+    reticulate::py_run_string("
+import torch
+torch.set_num_threads(1)
+torch.set_num_interop_threads(1)
+")
+    message("[INFO] PyTorch threads set to 1 for macOS stability.")
+  }
+  
+}
